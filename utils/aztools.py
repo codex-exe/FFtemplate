fe2b6d49a0c88b024e2cc57309585f7654dd711a
Alabhya Sharma <71889507+codex-exe@users.noreply.github.com>
2024-02-24 13:20:51 +0530

    Update aztools.py

--- utils/aztools.py
+++ utils/aztools.py
@@ -29,3 +29,22 @@ def submit_document_to_backend(payload, service, key):
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()
+
+def fetch_text(url: str):
+  try:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
+        soup.prettify()
+        return soup.get_text()[:2000]
+    else:
+        msg = f"Get url failed with status code {response.status_code}.\nURL: {url}\nResponse: " \
+              f"{response.text[:100]}"
+        print(msg)
+        return "No available content"
+  except Exception as e:
+      print("Get url failed with error: {}".format(e))
+      return "No available content"
